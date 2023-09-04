9ca7be8e5144f2e215ffe394f3172c7d5bb4e0c4
devkunalgaikwad <kunalgaikwad6390@gmail.com>
2023-09-04 08:50:41 +0530

    fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
    <>
      <div className='mx-[45px] my-[20vh] gap-y-4 flex flex-col max-w-[800px]'>
       <h1 className='font-bold  md:text-[70px] text-[50px] flex-wrap text-[white]'>Abu Dhabi Movers & Shifters</h1>
-      <h4 className='font-semibold md:text-[25px] text-[15px] text-white'>"Your Journey, Our Expertise: Seamless Moves Across Abu Dhabi"</h4>
+      <h4 className='font-semibold md:text-[25px] text-[15px] text-white'>Your Journey, Our Expertise: Seamless Moves Across Abu Dhabi</h4>
       <div className='w-auto m-2'>
         <Button variant="contained" className='bg-red-800 md:text-2xl text-lg'>Get a Free Quote Now.</Button>
       </div>
